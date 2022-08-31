19b8723f1732ff3218f97f9ba0a802f53b5766a9
Alanakcb <alanacantuaria@gmail.com>
2022-08-31 08:00:07 -0300

    oi

---
+++ database/migrations/2022_08_31_074513_create_precos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('precos', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()
+                ->onDelete('cascade')->onUpdate('cascade');
+            $table->float('valores');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('precos');
+    }
+};
